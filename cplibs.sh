acd21856d9d6a4e335cefd3f62051390b74a9e59
Andrew Miyaguchi <XDleader555@gmail.com>
2019-07-05 16:06:33 -0700

    Update cplibs script and checkout latest arduino-esp32 release - Checkout arduino-esp32 v1.0.3-rc1 0acf19a - Copy sdkconfig to reflect changes - update gitignore

--- cplibs.sh
+++ cplibs.sh
@@ -1,30 +1,51 @@
 #!/bin/bash
 
-rm ~/projects/esp32lib/lib/*
-rm ~/projects/esp32lib/ld/*
+# Get script location
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "Project Directory: $DIR"
 
-rm -r ~/projects/blink/build/bootloader
+if [ ! -d "$DIR/sdk" ]; then
+    # Create sdk directories
+    echo "Creating sdk directory"
+    mkdir $DIR/sdk
+    mkdir $DIR/sdk/ld
+    mkdir $DIR/sdk/lib
+else
+    # Empty directories
+    rm -f $DIR/sdk/ld/*
+    rm -f $DIR/sdk/lib/*
+    rm -f $DIR/sdk/sdkconfig
+fi
 
-find ~/projects/blink/build -name *.a -exec cp {} ~/projects/esp32lib/lib/ \;
-find ~/projects/blink/components/esp-face/lib -name *.a -exec cp {} ~/projects/esp32lib/lib/ \;
-find ~/projects/esp-idf/components/esp32 -name *.a -exec cp {} ~/projects/esp32lib/lib/ \;
-find ~/projects/esp-idf/components/bt -name *.a -exec cp {} ~/projects/esp32lib/lib/ \;
-cp ~/projects/esp-idf/components/newlib/lib/libc_nano.a ~/projects/esp32lib/lib/
-cp ~/projects/esp-idf/components/newlib/lib/libc.a ~/projects/esp32lib/lib/
+echo "Copying files..."
+# Copy compiled libraries
+find $DIR/build -type d -path ./build/bootloader -prune -o -name *.a -exec cp {} $DIR/sdk/lib/ \;
+find $DIR/components/esp-who/components/esp-face/lib -name *.a -exec cp {} $DIR/sdk/lib/ \;
+find $IDF_PATH/components/esp32 -name *.a -exec cp {} $DIR/sdk/lib/ \;
+find $IDF_PATH/components/bt -name *.a -exec cp {} $DIR/sdk/lib/ \;
+cp $IDF_PATH/components/newlib/lib/libc_nano.a $DIR/sdk/lib/
+cp $IDF_PATH/components/newlib/lib/libc.a $DIR/sdk/lib/
 
-find ~/projects/blink/build -name *.ld -exec cp {} ~/projects/esp32lib/ld/ \;
-find ~/projects/esp-idf/components/esp32/ld -maxdepth 1 -name *.ld  -exec cp {} ~/projects/esp32lib/ld/ \;
-find ~/projects/esp-idf/components/esp32/ld/wifi_iram_opt/ -maxdepth 1 -name *.ld  -exec cp {} ~/projects/esp32lib/ld/ \;
+# Copy Linker files
+find $DIR/build -type d -path ./build/bootloader -prune -o -name *.ld -exec cp {} $DIR/sdk/ld/ \;
+find $IDF_PATH/components/esp32/ld -maxdepth 1 -name *.ld  -exec cp {} $DIR/sdk/ld/ \;
+find $IDF_PATH/components/esp32/ld/wifi_iram_opt/ -maxdepth 1 -name *.ld  -exec cp {} $DIR/sdk/ld/ \;
 
-cp sdkconfig ~/projects/esp32lib/
+# Copy sdkconfig
+cp $DIR/sdkconfig $DIR/sdk/
 
-cd ~/projects/esp32lib/
-rm ./lib/libarduino.a 
-rm ./lib/libaws_iot.a
-rm ./lib/libesp-face.a
-rm ./lib/libidf_test.a
-rm ./lib/libunity.a
-rm ./lib/libmain.a
-rm ./lib/libesp_https_server.a
+# Cleanup sdk
+#rm -f $DIR/sdk/lib/libarduino.a 
+#rm -f $DIR/sdk/lib/libaws_iot.a
+#rm -f $DIR/sdk/lib/libesp-face.a
+#rm -f $DIR/sdk/lib/libidf_test.a
+#rm -f $DIR/sdk/lib/libunity.a
+#rm -f $DIR/sdk/lib/libmain.a
+#rm -f $DIR/sdk/lib/libesp_https_server.a
 
-zip -FSr /var/www/andrewmiyaguchi.com/html/build/esp32lib.zip ./
+# zip files
+echo "Zipping sdk..."
+cd $DIR/sdk
+zip -FSrq $DIR/arduino-esp32-libs.zip ./
+
+echo "Done! Replace the built in esp32 sdk folder with the one found here"
